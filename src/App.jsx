039a285c176e024c4f0db0d0a9a213dab3ab6721
てぃ <166586496+Madoyume@users.noreply.github.com>
2025-01-13 14:15:31 +0900

    [upd]再レンダリング周りの最適化(memo, useCallback)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./components/ChildArea";
 
 function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  console.log("Appがレンダリングされた！");
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input value={text} onChange={onChangeText} />
+        <br />
+        <br />
+        <button onClick={onClickOpen}>表示</button>
+        <ChildArea open={open} onClickClose={onClickClose} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ChildArea.jsx
@@ -0,0 +1,29 @@
+import React, { memo } from "react";
+
+const style = {
+  winth: "100%",
+  height: "200px",
+  backgroundColor: "khaki",
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンダリングされた！");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  console.log(data);
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
