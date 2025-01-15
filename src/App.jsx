e4c9ee8877fb3878e6a5aba3f375f1b71918bffb
てぃ <166586496+Madoyume@users.noreply.github.com>
2025-01-16 01:42:04 +0900

    [upd]Styled-Componentsの適用

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import { InlineStyle } from "./components/InlineStyle";
 import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
 
 function App() {
   return (
     <>
       <InlineStyle />
       <StyledJsx />
+      <StyledComponents />
     </>
   );
 }
