49dd4ae62b15e6987b6fe66969320831bddecbe9
てぃ <166586496+Madoyume@users.noreply.github.com>
2025-01-16 02:15:33 +0900

    [upd]React Routerの導入まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import React from "react";
-import { InlineStyle } from "./components/InlineStyle";
-import { StyledJsx } from "./components/StyledJsx";
-import { StyledComponents } from "./components/StyledComponents";
-import { Emotion } from "./components/Emotion";
+import Home from "./Home";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
 
 function App() {
   return (
     <>
-      <InlineStyle />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
+      <BrowserRouter>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <br />
+          <Link to="/page1">Page1</Link>
+          <br />
+          <Link to="/page2">Page2</Link>
+          <br />
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
