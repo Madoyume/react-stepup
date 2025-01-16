d4ae111d40a97e1851b21b001a4e6db221667106
てぃ <166586496+Madoyume@users.noreply.github.com>
2025-01-17 02:59:10 +0900

    [upd]ルート定義の分割方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Page1Wrapper from "./Page1Wrapper";
-import Page1DetailA from "./Page1DetailA";
-import Page1DetailB from "./Page1DetailB";
-import Page2 from "./Page2";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 
 import "./App.css";
 
@@ -20,14 +16,7 @@ function App() {
           <Link to="/page2">Page2</Link>
           <br />
         </div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/page1" element={<Page1Wrapper />}>
-            <Route path="detailA" element={<Page1DetailA />} />
-            <Route path="detailB" element={<Page1DetailB />} />
-          </Route>
-          <Route path="/page2" element={<Page2 />} />
-        </Routes>
+        <Router />
       </BrowserRouter>
     </>
   );
